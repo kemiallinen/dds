275473d3a953a40b92e6f1c4dd38238c57a5812e
kemiallinen <35116031+kemiallinen@users.noreply.github.com>
2020-01-15 19:01:01 +0100

    * added spectral and agglomerative clustering

--- clustering.py
+++ clustering.py
@@ -1,24 +1,26 @@
 import pandas as pd
 import collections
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, SpectralClustering, AgglomerativeClustering
 from pprint import pprint
 from sklearn.feature_extraction.text import HashingVectorizer
-import numpy as np
 
 
 data = pd.read_csv('out.csv', sep=',')
-formulas = data['formula'].sample(n=200, random_state=1)
+formulas = data['formula'].sample(n=100, random_state=1)
 vectorizer = HashingVectorizer(n_features=2**4,
                                ngram_range=(1, 3),
                                analyzer='char')
 X = vectorizer.fit_transform(formulas)
-km_model = KMeans(n_clusters=10)
-km_model.fit(X)
+# km_model = KMeans(n_clusters=7).fit(X)
+# sc_model = SpectralClustering(n_clusters=7).fit(X)
+ac_model = AgglomerativeClustering(n_clusters=7).fit(X.toarray())
+
 clustering = collections.defaultdict(list)
 
 formulas_np = formulas.to_numpy()
+model = ac_model
 
-for idx, label in enumerate(km_model.labels_):
+for idx, label in enumerate(model.labels_):
     clustering[label].append(formulas_np[idx])
 
 pprint(dict(clustering))
