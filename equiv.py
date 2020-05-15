c532a095c32498c9dc3139a976cf05c86c1c60a4
kemiallinen <d.niemiec@amu.edu.pl>
2020-05-15 12:13:54 +0200

    major 2020 patch

--- equiv.py
+++ equiv.py
@@ -1,18 +1,8 @@
 import re
-from itertools import chain
-import random
-import matplotlib.pyplot as plt
+from termcolor import cprint
 import networkx as nx
-# from networkx.drawing.nx_agraph import graphviz_layout
-
-
-class Dictlist(dict):
-    def __setitem__(self, key, value):
-        try:
-            self[key]
-        except KeyError:
-            super(Dictlist, self).__setitem__(key, [])
-        self[key].append(value)
+from networkx.drawing.nx_agraph import graphviz_layout
+import matplotlib.pyplot as plt
 
 
 class Proofer():
@@ -22,8 +12,7 @@ class Proofer():
         self.ss = ss
         self.op = op
         self.lang = 'ABΓΔ'
-        self.rule_dict = Dictlist()
-        self.inv_dict = {'A': '', 'B': ''}
+        self.inv_dict = {'A': [], 'B': []}
         self.cut_formula = ''
         self.possible_cuts = 'AB'
         self.nodes_checked = []
@@ -37,37 +26,38 @@ class Proofer():
         print('level = {}'.format(level))
         print('sequent = {}'.format(self.sequent))
         print('current parent = {}'.format(actual_parent))
+        print('nodes checked = {}'.format(self.nodes_checked))
         print('*************\n')
 
-        if self.check_if_tautology():
+        if '~' in self.sequent:
+            # TODO: fix possible bug in eg. ~p o q
+            self.nodes_checked.append('{}'.format(self.sequent))
+            actual_parent = self.sequent
+            self.negation_remover()
+            self.G.add_node(self.sequent)
+            self.G.add_edge(actual_parent, self.sequent)
+            level += 1
             print('\n*************')
-            print('{} is tautology'.format(self.sequent))
+            print('[negation removed]')
+            print('level = {}'.format(level))
+            print('sequent = {}'.format(self.sequent))
+            print('current parent = {}'.format(actual_parent))
+            print('*************\n')
+        print(self.sequent)
+        if self.check_if_axiom():
+            print('\n*************')
+            print('{} is axiom'.format(self.sequent))
             print('current parent = {}'.format(actual_parent))
             print('node is closed')
             print('*************\n')
 
         else:
             self.nodes_checked.append('{}'.format(self.sequent))
-
-            if '~' in self.sequent:
-                actual_parent = self.sequent
-                self.negation_remover()
-                self.G.add_node(self.sequent)
-                self.G.add_edge(actual_parent, self.sequent)
-                level += 1
-                self.nodes_checked.append('{}'.format(self.sequent))
-                print('\n*************')
-                print('[negation removed]')
-                print('level = {}'.format(level))
-                print('sequent = {}'.format(self.sequent))
-                print('current parent = {}'.format(actual_parent))
-                print('*************\n')
-
             self.seq2base()
 
             print('\n*************')
             print('sequent fitted = {}'.format(self.sequent))
-            print('fitting dict = {}'.format(self.rule_dict))
+            print('fitting dict = {}'.format(self.inv_dict))
 
             if self.sequent in self.rules:
                 print('[solutions from rules]')
@@ -78,18 +68,15 @@ class Proofer():
                 solutions = self.cut()
 
             if type(solutions) == str:
-                solutions = list(solutions)
+                # solutions = list(solutions)
+                solutions = [solutions]
 
             print('[solutions fitted] = {}'.format(solutions))
-
+            level += 1
             for n_sol, solution in enumerate(solutions):
                 print('solution {} = {}'.format(n_sol+1, self.base2seq(solution)))
-            print('*************\n')
-
-            actual_parent = self.base2seq(self.sequent)
-            level += 1
-
             for solution in solutions:
+                actual_parent = self.base2seq(self.sequent)
                 self.sequent = self.base2seq(solution)
                 if not (self.sequent in self.nodes_checked):
                     self.pipeline(level, actual_parent)
@@ -101,7 +88,7 @@ class Proofer():
                     print('level = {}'.format(level))
                     print('*************\n')
 
-    def check_if_tautology(self):
+    def check_if_axiom(self):
         if set(re.split(',', self.sequent.split(self.ss)[0])) & set(re.split(',', self.sequent.split(self.ss)[1])):
             return True
         else:
@@ -115,108 +102,145 @@ class Proofer():
         self.sequent = ss.join(sequent)
 
     def negs_replace(self, negs, sequent, n):
+        # can be thrown out of the class
         for neg in negs:
-            if len(neg) == 2:
-                if sequent[abs(n - 1)]:
-                    sequent[abs(n - 1)] += ',' + neg[1:]
+            if {neg, '~(' + neg + ')'}.intersection(set([item for side in sequent for item in side.split(',')])):
+                if len(neg) == 2:
+                    to_replace = neg
+                    neg = neg[1]
                 else:
-                    sequent[abs(n - 1)] += neg[1:]
-                to_replace = neg
-            else:
-                if sequent[abs(n - 1)]:
-                    sequent[abs(n - 1)] += ',' + neg
+                    to_replace = '~(' + neg + ')'
+                if sequent[int(not n)]:
+                    if neg not in sequent[int(not n)].split(','):
+                        sequent[int(not n)] += ',' + neg
                 else:
-                    sequent[abs(n - 1)] += neg
-                to_replace = '~(' + neg + ')'
+                    sequent[int(not n)] += neg
 
-            sequent[n] = sequent[n].replace(to_replace, '')
-            sequent[n] = ','.join(filter(None, re.split(',', sequent[n])))
+                sequent[n] = sequent[n].replace(to_replace, '')
+                sequent[n] = ','.join(filter(None, re.split(',', sequent[n])))
 
         return sequent
 
     def seq2base(self):
         sequent = re.split(self.ss, self.sequent)
-
-        dissolve = self.update_dict(sequent)
-
-        for n, side in enumerate(sequent):
-            if n == 0:
-                noise = 'Γ'
+        elts = [elt for side in sequent for elt in side.split(',')]
+        longest_elt = str(max(elts, key=len))
+        dissolve = re.findall('\((.*?)\)', longest_elt)
+        # print('[1]', dissolve)
+        if not dissolve:
+            dissolve = re.split(self.op, longest_elt)
+            # print('[2]', dissolve)
+        if len(dissolve) == 1:
+            dissolve = [elt for elt in ','.join(sequent).split(',') if elt][:2]
+        # elif len(dissolve) == 1:
+        #     dissolve = re.split(self.op, dissolve[0])
+        #     print('[3]', dissolve)
+        # if len(dissolve) == 1:
+        #     dissolve = sequent[:]
+        #     print('[4]', dissolve)
+        sequent = [re.sub(f"\({dissolve[0]}\){self.op}\({dissolve[1]}\)|{dissolve[0]}{self.op}{dissolve[1]}",
+                          f"A{self.op}B", side) for side in sequent]
+        # print(sequent)
+        # print(dissolve)
+        for w1, w2 in zip(dissolve, self.lang[:len(dissolve)]):
+            if (not (self.inv_dict[w2] == '(' + w1 + ')')) and (not (self.inv_dict[w2] == w1)):
+                self.possible_cuts = 'AB'
+                # self.inv_dict = {'A': [], 'B': []}
+            if len(w1) > 1:
+                self.inv_dict[w2] = ['(' + w1 + ')']
             else:
-                noise = 'Δ'
-
-            for num_elt, elt in enumerate(side):
-                if len(self.rule_dict) > 1:
-                    side[num_elt] = re.sub(dissolve[0], self.rule_dict[dissolve[0]][0], elt)
-                    side[num_elt] = re.sub(dissolve[1], self.rule_dict[dissolve[1]][0], side[num_elt])
-                    side[num_elt] = re.sub('\(|\)', '', side[num_elt])
+                self.inv_dict[w2] = [w1]
+            sequent = [re.sub(f"{w1}", f"{w2}", side) for side in sequent]
+        # print(self.inv_dict)
+        # print(sequent)
+        sequent = [re.sub(r"\((.{0,3})\)", r"\1", side) for side in sequent]
+        for n, side in enumerate(sequent):
+            noise = self.lang[n + 2]
+            # self.inv_dict[noise] = re.findall(f'[^{self.lang}~,]+', side)
+            self.inv_dict[noise] = list(filter(lambda x: not set(x) <= set(self.lang+self.op), side.split(',')))
+            # if op in self.inv_dict[noise]:
+            #     self.inv_dict[noise].remove(op)
+            self.inv_dict[noise].sort(key=len, reverse=True)
+            for elt in self.inv_dict[noise]:
+                side = re.sub('~?' + re.escape(elt), '', side)
+            side = re.sub(',{2,}', ',', side)
+            side = re.sub('^,*|,*$', '', side)
+            if side:
+                if n:
+                    side = noise + ',' + side
                 else:
-                    side[num_elt] = re.sub(dissolve[0], self.rule_dict[dissolve[0]][0], elt, 1)
-                    side[num_elt] = re.sub(dissolve[1], self.rule_dict[dissolve[1]][1], side[num_elt], 1)
-
-                if not any(ch in side[num_elt] for ch in self.lang):
-                    if side[num_elt]:
-                        self.rule_dict[side[num_elt]] = noise
-                        self.inv_dict[noise] = side[num_elt]
-                    side[num_elt] = noise
-
-            sequent[n] = ','.join(side)
-            if not any(ch in sequent[n] for ch in 'ΓΔ'):
-                sequent[n] += ',' + noise
-            sequent[n] = self.sort_formulas(sequent[n], n)
-
+                    side = side + ',' + noise
+            else:
+                side = noise
+            sequent[n] = self.sort_formulas(side, n)
+        # print(sequent)
+#########################################################
+        # dissolve = self.update_dict(sequent)
+        #
+        # for n, side in enumerate(sequent):
+        #     print('n = ', n, ' side = ', side)
+        #     if n == 0:
+        #         noise = 'Γ'
+        #     else:
+        #         noise = 'Δ'
+        #
+        #     for num_elt, elt in enumerate(side):
+        #         print('num_elt = ', num_elt, ' elt = ', elt)
+        #         if len(self.rule_dict) > 1:
+        #             side[num_elt] = re.sub(dissolve[0], self.rule_dict[dissolve[0]][0], elt)
+        #             side[num_elt] = re.sub(dissolve[1], self.rule_dict[dissolve[1]][0], side[num_elt])
+        #             side[num_elt] = re.sub('\(|\)', '', side[num_elt])
+        #         else:
+        #             side[num_elt] = re.sub(dissolve[0], self.rule_dict[dissolve[0]][0], elt, 1)
+        #             side[num_elt] = re.sub(dissolve[1], self.rule_dict[dissolve[1]][1], side[num_elt], 1)
+        #
+        #         if not any(ch in side[num_elt] for ch in self.lang):
+        #             if side[num_elt]:
+        #                 self.rule_dict[side[num_elt]] = noise
+        #                 self.inv_dict[noise] = side[num_elt]
+        #             side[num_elt] = noise
+        #
+        #     sequent[n] = ','.join(side)
+        #     if not any(ch in sequent[n] for ch in 'ΓΔ'):
+        #         sequent[n] += ',' + noise
+        #     sequent[n] = self.sort_formulas(sequent[n], n)
+        # print(self.inv_dict)
         self.sequent = self.ss.join(sequent)
 
     def base2seq(self, solution):
-        for ch in 'ΓΔ':
-            if not ch in self.inv_dict:
-                self.inv_dict[ch] = ''
-        sol_seq = []
-        for elt in re.split(self.ss, multi_replace(self.inv_dict, solution)):
-            sol_side = []
-            for splt in re.split(',', elt):
-                if (len(re.findall('\((.*?)\)', splt)) == 1) and (not splt[0] == '~'):
-                    sol_side.append(re.sub('\(|\)', '', splt))
-                else:
-                    sol_side.append(splt)
-            sol_seq.append(','.join(filter(None, sol_side)))
-        return self.ss.join(sol_seq)
+        for ch in self.lang[::-1]:
+            solution = re.sub(ch, ','.join(self.inv_dict[ch]), solution)
+        solution = solution.split(self.ss)
+        for n_side, side in enumerate(solution):
+            side = side.split(',')
+            for n_elt, elt in enumerate(side):
+                if len(elt) == 5:
+                    side[n_elt] = elt[1:4]
+            solution[n_side] = ','.join(side)
+        solution = self.ss.join(solution)
+        return re.sub(f',?{self.ss},?', self.ss, solution)
 
-    def update_dict(self, sequent):
-        for n, side in enumerate(sequent):
-            sequent[n] = re.split(',', side)
-        lens = []
-        elts = []
-        for elt in chain.from_iterable(sequent):
-            lens.append(len(elt))
-            elts.append(elt)
-        if (len(set(lens)) == 1) and not(len(set(re.findall('[a-z]', ''.join(elts))))<=2):
-            dissolve = random.sample(elts, 2)
-        else:
-            longest_elt = max(chain.from_iterable(sequent), key=len)
-            dissolve = re.findall('\((.*?)\)', longest_elt)
-            if not dissolve:
-                dissolve = re.split(self.op, longest_elt)
-        dissolve = dissolve[::-1]
-        self.rule_dict = Dictlist()
-        for w1, w2 in zip(dissolve, self.lang[:len(dissolve)]):
-            if not (self.inv_dict[w2] == '(' + w1 + ')'):
-                if not (self.inv_dict[w2] == w1):
-                    self.possible_cuts = 'AB'
-
-            if len(w1) > 1:
-                self.rule_dict[w1] = w2
-                self.inv_dict[w2] = '(' + w1 + ')'
-            else:
-                self.rule_dict[w1] = w2
-                self.inv_dict[w2] = w1
-        return dissolve
+        # for ch in 'ΓΔ':
+        #     if not ch in self.inv_dict:
+        #         self.inv_dict[ch] = ''
+        # sol_seq = []
+        # for elt in re.split(self.ss, multi_replace(self.inv_dict, solution)):
+        #     sol_side = []
+        #     for splt in re.split(',', elt):
+        #         if (len(re.findall('\((.*?)\)', splt)) == 1) and (not splt[0] == '~'):
+        #             sol_side.append(re.sub('\(|\)', '', splt))
+        #         else:
+        #             sol_side.append(splt)
+        #     sol_seq.append(','.join(filter(None, sol_side)))
+        # return self.ss.join(sol_seq)
 
     def sort_formulas(self, side, num_side):
         if num_side == 0:
-            sort_ord = {'A': 1, 'B': 2, 'A' + op + 'B': 3, 'Γ': 4}
+            sort_ord = {'A': 1, 'B': 2, 'A' + self.op + 'B': 3, 'Γ': 4}
         else:
-            sort_ord = {'Δ': 1, 'A': 2, 'B': 3, 'A' + op + 'B': 4, 'B' + op + 'A': 5}
+            sort_ord = {'Δ': 1, 'A': 2, 'B': 3,
+                        'A' + self.op + 'B': 4,
+                        'B' + self.op + 'A': 5}
         to_sort = re.split(',', side)
         to_sort_num = []
         for elt in to_sort:
@@ -254,10 +278,12 @@ def seq_format(sequent):
 
 
 # '=' is the equivalence for the purpose of a user-friendly input
-testSeqs = ['p->p=p',
+testSeqs = ['->p=p',
+            'p->p=p',
             '->p=p,p',
             '~p->p=p',
             '->p=p,~p',
+            '->(p=q)=(q=p)',
             'p=q->p=r,(q=r)=(p=r)',
             'p=q,p=r->(q=r)=(p=r)',
             'p=q->~(p=r),(q=r)=(p=r)',
@@ -296,22 +322,31 @@ rulesNoise = {'B,A≡B,Γ⇒Δ':      ['A,B,Γ⇒Δ',
               'Γ⇒Δ,A,B':        'A≡B,Γ⇒Δ,B'}
 ss = '⇒'
 op = '≡'
+#
+# test_seq = testSeqs[6]
+# test_seq = seq_format(test_seq)
+# obj = Proofer(test_seq, rulesNoise, ss, op)
+# obj.pipeline()
 
-# test_seq = testSeqs[4]
-# test_seq = '->(p=q)=(q=p)'
-test_seq = '->p=p'
-test_seq = seq_format(test_seq)
-obj = Proofer(test_seq, rulesNoise, ss, op)
-obj.pipeline()
+# test_seq = seq_format('p=q->p=r,(q=r)=(p=r)')
+# obj = Proofer(test_seq, rulesNoise, ss, op)
+# obj.pipeline()
 
-# nx.nx_agraph.write_dot(obj.G, 'test.dot')
+for test_seq in testSeqs:
+    test_seq = seq_format(test_seq)
+    obj = Proofer(test_seq, rulesNoise, ss, op)
+    obj.pipeline()
+    cprint('\n' + '#'*40, "blue", end='\n\n')
 
-# same layout using matplotlib with no labels
-# plt.title('draw_networkx')
-# pos = graphviz_layout(obj.G, prog='dot')
-# nx.draw(obj.G, pos, with_labels=False, arrows=False)
-nx.draw(obj.G, with_labels=True)
-plt.show()
+    nx.nx_agraph.write_dot(obj.G, 'test.dot')
+
+    # same layout using matplotlib with no labels
+    plt.title('draw_networkx')
+    pos = graphviz_layout(obj.G, prog='dot')
+    nx.draw(obj.G, pos, with_labels=False, arrows=False)
+    nx.draw(obj.G, with_labels=True)
+    plt.show()
+    input()
 
 # TODO: save output to tex file
 #
