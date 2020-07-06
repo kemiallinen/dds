444ff5af014dd72bd63788f387a5d51ced069040
kemiallinen <35116031+kemiallinen@users.noreply.github.com>
2020-07-06 18:34:23 +0200

    trying to visualize this hot mess

--- another_algorithm.py
+++ another_algorithm.py
@@ -1,5 +1,10 @@
 import re
 import networkx as nx
+import pandas as pd
+# import igraph
+# import plotly.graph_objects as go
+# import plotly.express as px
+# from igraph import Graph, EdgeSeq
 import matplotlib.pyplot as plt
 import random
 
@@ -73,53 +78,30 @@ def check_if_axiom(sequent):
         return False
 
 
-def hierarchy_pos(G, root=None, width=1., vert_gap=0.2, vert_loc=0, leaf_vs_root_factor=0.5):
+def hierarchy_pos(G, root=None, width=1., vert_gap = 0.2, vert_loc = 0, xcenter = 0.5):
+
     '''
-    src: https://epidemicsonnetworks.readthedocs.io/en/latest/_modules/EoN/auxiliary.html#hierarchy_pos
+    From Joel's answer at https://stackoverflow.com/a/29597209/2966723.
+    Licensed under Creative Commons Attribution-Share Alike
 
     If the graph is a tree this will return the positions to plot this in a
     hierarchical layout.
 
-    Based on Joel's answer at https://stackoverflow.com/a/29597209/2966723,
-    but with some modifications.
+    G: the graph (must be a tree)
 
-    We include this because it may be useful for plotting transmission trees,
-    and there is currently no networkx equivalent (though it may be coming soon).
+    root: the root node of current branch
+    - if the tree is directed and this is not given,
+      the root will be found and used
+    - if the tree is directed and this is given, then
+      the positions will be just for the descendants of this node.
+    - if the tree is undirected and not given,
+      then a random choice will be used.
 
-    There are two basic approaches we think of to allocate the horizontal
-    location of a node.
+    width: horizontal space allocated for this branch - avoids overlap with other branches
 
-    - Top down: we allocate horizontal space to a node.  Then its ``k``
-      descendants split up that horizontal space equally.  This tends to result
-      in overlapping nodes when some have many descendants.
-    - Bottom up: we allocate horizontal space to each leaf node.  A node at a
-      higher level gets the entire space allocated to its descendant leaves.
-      Based on this, leaf nodes at higher levels get the same space as leaf
-      nodes very deep in the tree.
+    vert_gap: gap between levels of hierarchy
 
-    We use use both of these approaches simultaneously with ``leaf_vs_root_factor``
-    determining how much of the horizontal space is based on the bottom up
-    or top down approaches.  ``0`` gives pure bottom up, while 1 gives pure top
-    down.
-
-
-    :Arguments:
-
-    **G** the graph (must be a tree)
-
-    **root** the root node of the tree
-    - if the tree is directed and this is not given, the root will be found and used
-    - if the tree is directed and this is given, then the positions will be
-      just for the descendants of this node.
-    - if the tree is undirected and not given, then a random choice will be used.
-
-    **width** horizontal space allocated for this branch - avoids overlap with other branches
-
-    **vert_gap** gap between levels of hierarchy
-
-    **vert_loc** vertical location of root
-
-    **leaf_vs_root_factor**
+    vert_loc: vertical location of root
 
     xcenter: horizontal location of root
     '''
@@ -128,13 +110,11 @@ def hierarchy_pos(G, root=None, width=1., vert_gap=0.2, vert_loc=0, leaf_vs_root
 
     if root is None:
         if isinstance(G, nx.DiGraph):
-            root = next(iter(nx.topological_sort(G)))  # allows back compatibility with nx version 1.11
+            root = next(iter(nx.topological_sort(G)))  #allows back compatibility with nx version 1.11
         else:
             root = random.choice(list(G.nodes))
 
-    def _hierarchy_pos(G, root, leftmost, width, leafdx=0.2, vert_gap=0.2, vert_loc=0,
-                       xcenter=0.5, rootpos=None,
-                       leafpos=None, parent=None):
+    def _hierarchy_pos(G, root, width=1., vert_gap = 0.2, vert_loc = 0, xcenter = 0.5, pos = None, parent = None):
         '''
         see hierarchy_pos docstring for most arguments
 
@@ -143,57 +123,25 @@ def hierarchy_pos(G, root=None, width=1., vert_gap=0.2, vert_loc=0, leaf_vs_root
 
         '''
 
-        if rootpos is None:
-            rootpos = {root: (xcenter, vert_loc)}
+        if pos is None:
+            pos = {root:(xcenter,vert_loc)}
         else:
-            rootpos[root] = (xcenter, vert_loc)
-        if leafpos is None:
-            leafpos = {}
+            pos[root] = (xcenter, vert_loc)
         children = list(G.neighbors(root))
-        leaf_count = 0
         if not isinstance(G, nx.DiGraph) and parent is not None:
             children.remove(parent)
-        if len(children) != 0:
-            rootdx = width / len(children)
-            nextx = xcenter - width / 2 - rootdx / 2
+        if len(children)!=0:
+            dx = width/len(children)
+            nextx = xcenter - width/2 - dx/2
             for child in children:
-                nextx += rootdx
-                rootpos, leafpos, newleaves = _hierarchy_pos(G, child, leftmost + leaf_count * leafdx,
-                                                             width=rootdx, leafdx=leafdx,
-                                                             vert_gap=vert_gap, vert_loc=vert_loc - vert_gap,
-                                                             xcenter=nextx, rootpos=rootpos, leafpos=leafpos,
-                                                             parent=root)
-                leaf_count += newleaves
+                nextx += dx
+                pos = _hierarchy_pos(G,child, width = dx, vert_gap = vert_gap,
+                                    vert_loc = vert_loc-vert_gap, xcenter=nextx,
+                                    pos=pos, parent = root)
+        return pos
 
-            leftmostchild = min((x for x, y in [leafpos[child] for child in children]))
-            rightmostchild = max((x for x, y in [leafpos[child] for child in children]))
-            leafpos[root] = ((leftmostchild + rightmostchild) / 2, vert_loc)
-        else:
-            leaf_count = 1
-            leafpos[root] = (leftmost, vert_loc)
-        #        pos[root] = (leftmost + (leaf_count-1)*dx/2., vert_loc)
-        #        print(leaf_count)
-        return rootpos, leafpos, leaf_count
 
-    xcenter = width / 2.
-    if isinstance(G, nx.DiGraph):
-        leafcount = len([node for node in nx.descendants(G, root) if G.out_degree(node) == 0])
-    elif isinstance(G, nx.Graph):
-        leafcount = len([node for node in nx.node_connected_component(G, root) if G.degree(node) == 1 and node != root])
-    rootpos, leafpos, leaf_count = _hierarchy_pos(G, root, 0, width,
-                                                  leafdx=width * 1. / leafcount,
-                                                  vert_gap=vert_gap,
-                                                  vert_loc=vert_loc,
-                                                  xcenter=xcenter)
-    pos = {}
-    for node in rootpos:
-        pos[node] = (
-        leaf_vs_root_factor * leafpos[node][0] + (1 - leaf_vs_root_factor) * rootpos[node][0], leafpos[node][1])
-    #    pos = {node:(leaf_vs_root_factor*x1+(1-leaf_vs_root_factor)*x2, y1) for ((x1,y1), (x2,y2)) in (leafpos[node], rootpos[node]) for node in rootpos}
-    xmax = max(x for x, y in pos.values())
-    for node in pos:
-        pos[node] = (pos[node][0] * width / xmax, pos[node][1])
-    return pos
+    return _hierarchy_pos(G, root, width, vert_gap, vert_loc, xcenter)
 
 
 class Sequent:
@@ -231,16 +179,21 @@ class Prover:
     def __init__(self):
         self.ss = '⇒'
         self.conn = '≡'
-        self.tree = nx.DiGraph()
+        self.tree = pd.DataFrame(columns=['depth', 'parent', 'value'])
         self.longest_objects = []
         self.rules = {'Γ⇒Δ,A≡B,A,B': 'B,Γ⇒Δ,A≡B,B', # (05) ↑
                       'B,A,Γ⇒Δ,A≡B': 'A,Γ⇒Δ,A,A≡B', # (05) ↓
                       'B,A≡B,Γ⇒Δ,A': 'B,A≡B,Γ⇒Δ,B', # (08) ↑
                       'A,A≡B,Γ⇒Δ,B': 'A,A≡B,Γ⇒Δ,A'} # (08) ↓
 
-    def pipeline(self, seq):
+    def pipeline(self, seq, depth=0):
         print(f'current sequent = {seq.sequent}')
-        self.tree.add_node(seq.sequent)
+        # self.tree.add_node(seq.sequent)
+        if self.tree.empty:
+            self.tree = self.tree.append({'depth': depth,
+                                          'parent': 'ROOT',
+                                          'value': seq.sequent},
+                                         ignore_index=True)
 
         # remove negations
         if '~' in seq.sequent:
@@ -269,7 +222,7 @@ class Prover:
                 seq.base = 'A≡B,Γ⇒Δ'
 
             # perform cut twice (and check if axiom after each cut)
-            self.recursive_cut(seq)
+            self.recursive_cut(seq, depth=depth)
 
         else:
             print('******** NA ********')
@@ -292,19 +245,23 @@ class Prover:
                 sol = Sequent(sol, banned=set(self.longest_objects))
                 sol.base = base
                 sol.inv_dict = parent_sequent.inv_dict
-                self.tree.add_node(sol.sequent)
-                self.tree.add_edge(parent_sequent.sequent, sol.sequent)
+                # self.tree.add_node(sol.sequent)
+                # self.tree.add_edge(parent_sequent.sequent, sol.sequent)
+                self.tree = self.tree.append({'depth': depth,
+                                              'parent': parent_sequent.sequent,
+                                              'value': sol.sequent},
+                                             ignore_index=True)
                 if check_if_axiom(sol.sequent):
                     print('\t' * depth, end='')
                     print(f'Solution: {sol.sequent} is an axiom.')
                 else:
                     print('\t' * depth, end='')
                     print(f'Solution: {sol.sequent} is NOT an axiom.')
-                    self.recursive_cut(sol, possible_cuts[1:], depth)
+                    self.recursive_cut(sol, possible_cuts[1:], depth=depth)
         else:
-            self.from_rules(parent_sequent, depth)
+            self.from_rules(parent_sequent, depth=depth)
 
-    def from_rules(self, parent_sequent, depth):
+    def from_rules(self, parent_sequent, depth=0):
         depth += 1
         if parent_sequent.base in self.rules.keys():
             print('\t' * depth, end='')
@@ -317,8 +274,12 @@ class Prover:
                               banned=set(self.longest_objects))
             out_seq.base = out_base
 
-            self.tree.add_node(out_seq.sequent)
-            self.tree.add_edge(parent_sequent.sequent, out_seq.sequent)
+            # self.tree.add_node(out_seq.sequent)
+            # self.tree.add_edge(parent_sequent.sequent, out_seq.sequent)
+            self.tree = self.tree.append({'depth': depth,
+                                          'parent': parent_sequent.sequent,
+                                          'value': out_seq.sequent},
+                                         ignore_index=True)
 
             if check_if_axiom(out_seq.sequent):
                 print('\t' * depth, end='')
@@ -330,7 +291,7 @@ class Prover:
             print('\t' * depth, end='')
             print(f'No solution found for {parent_sequent.sequent}')
             print('*'*20)
-            self.pipeline(parent_sequent)
+            self.pipeline(parent_sequent, depth=depth)
 
 
 testSeqs = ['->p=p',
@@ -347,14 +308,31 @@ for testseq in testSeqs:
     prvr = Prover()
     seq_init = Sequent(testseq)
     prvr.pipeline(seq_init)
-    G = nx.DiGraph()
-    G.add_nodes_from(prvr.tree)
-    G.add_edges_from(prvr.tree.edges)
     print()
-    pos = hierarchy_pos(G)
-    nx.draw(G, pos=pos, with_labels=True)
+    print(prvr.tree[['parent', 'value']])
+    ts = [tuple(x) for x in prvr.tree[['parent', 'value']].values]
+    print(ts)
+    # Gm = Graph.TupleList(ts, directed=True)
+    # igraph.plot(Gm)
+    G = nx.DiGraph()
+    G.add_edges_from(ts)
+    print(G)
+    # pos = hierarchy_pos(G, 'ROOT')
+    nx.draw(G, with_labels=True)
     plt.savefig('out_graphs\\' + seq_init.sequent + '.png')
-    plt.close()
+
+    # https://stackoverflow.com/questions/57512155/how-to-draw-a-tree-more-beautifully-in-networkx
+
+    # g = igraph.gra
+    # fig = px.treemap(1/prvr.tree['depth'], parents=prvr.tree['parent'], values=prvr.tree['value'])
+    # fig = go.Figure()
+    # fig.add_trace(go.Treemap(parents=prvr.tree['parent'], values=prvr.tree['value']))
+    # fig.show()
+    break
+    # print('*'*30)
+    # input()
+    # print()
+
 
 # prvr = Prover()
 # seq_init = Sequent(testSeqs[6])
